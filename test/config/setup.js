c92ca0cf7b7811f7b3857f4ecbebb7f4a4917291
Mark Polak <mark@thedutchies.com>
2015-07-07 22:29:13 +0200

    Add sandboxSetup for global setup

--- test/config/setup.js
+++ test/config/setup.js
@@ -1,21 +1,8 @@
 'use strict';
 /* globals require global */
 
-let chai = require('chai');
-let sinon = require('sinon');
-let sinonChai = require('sinon-chai');
-chai.use(sinonChai);
+global.chai = require('chai');
+global.sinon = require('sinon');
+global.chai.use(require('sinon-chai'));
 
 global.expect = chai.expect;
-
-let sandbox;
-
-beforeEach(() => {
-    sandbox = sinon.sandbox.create();
-    global.spy = sandbox.spy.bind(sandbox);
-    global.stub = sandbox.stub.bind(sandbox);
-});
-
-afterEach(() => {
-   sandbox.restore();
-});
---
+++ test/config/setupSandbox.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let sinon = require('sinon');
+
+var sandbox;
+
+beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+    global.spy = sandbox.spy.bind(sandbox);
+    global.stub = sandbox.stub.bind(sandbox);
+});
+
+afterEach(() => {
+    sandbox.restore();
+});
