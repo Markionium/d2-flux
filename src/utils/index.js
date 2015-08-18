39d387ade32db0e260b55830a6d88d3bf4faaaf6
Mark Polak <mark@thedutchies.com>
2015-08-18 14:39:12 +0200

    Remove unused utils

--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export function camelToUnderscore(propertyName) {
-    return propertyName.replace(/([a-z])([A-Z])/g, '$1_$2').toUpperCase();
-}
-
-export function createSymbolOnObjectForKey(key) {
-    this[key] = Symbol(key);
-}
